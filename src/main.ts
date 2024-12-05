10bc226ba4fb1183a04238d0aaed5e514a940fe1
atsushi-h <atsushi.1213eng@gmail.com>
2024-12-05 20:04:58 +0900

    Prismaモジュール作成

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Request } from 'express';
+import * as cookieParser from 'cookie-parser';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // DTO、バリデーションの設定
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  // CORS
+  app.enableCors({
+    credentials: true,
+    origin: ['http:localhost:3000'],
+  });
+
+  // cookie
+  app.use(cookieParser());
+
   await app.listen(3005);
 }
 bootstrap();
