c9434756c0f6dcb108043fbede5d27f68c2b2999
atsushi-h <atsushi.1213eng@gmail.com>
2025-01-18 09:45:49 +0900

    パスを修正

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   // CORS
   app.enableCors({
     credentials: true,
-    origin: ['http:localhost:3000'],
+    origin: ['http://localhost:3000'],
   });
 
   // cookie
