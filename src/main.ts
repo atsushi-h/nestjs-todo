a0489fccdf15c9b835d1fd74f296f8001eccd205
atsushi-h <atsushi.1213eng@gmail.com>
2025-02-16 06:54:41 +0900

    CI設定（動作確認）

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { Request } from 'express';
 import * as cookieParser from 'cookie-parser';
 import * as csurf from 'csurf';
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
+import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -22,21 +25,34 @@ async function bootstrap() {
   app.use(
     csurf({
       cookie: { httpOnly: true, secure: true },
-      value: (req: Request) => {
-        return req.header('csrf-token');
-      },
+      value: (req: Request) => req.header('csrf-token'),
     }),
   );
 
-  // Swagger
+  // Swagger 設定
   const config = new DocumentBuilder()
     .setTitle('Todo API Document')
     .setDescription('Todo API description')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+
+  if (process.env.GENERATE_SWAGGER_YAML === 'true') {
+    // CI時はyamlファイルに出力
+    const yamlData = yaml.dump(document);
+    fs.writeFileSync(
+      path.join(__dirname, '..', 'docs', 'swagger.yaml'),
+      yamlData,
+      'utf8',
+    );
+
+    process.exit(0);
+  } else {
+    // ローカル環境では Swagger UI を起動
+    SwaggerModule.setup('api', app, document);
+  }
 
   await app.listen(process.env.PORT || 3005);
 }
+
 bootstrap();
