8f2e4600aa22bbbcecbc4bf882af2aee778542a1
atsushi-h <atsushi.1213eng@gmail.com>
2025-02-16 07:17:40 +0900

    CI時のエラー修正

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -5,10 +5,13 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor(private readonly config: ConfigService) {
-    if (process.env.GENERATE_SWAGGER_YAML === 'true') {
-      // CIのyamlファイル生成時はDBに接続しない
-      return;
-    }
-    super({ datasources: { db: { url: config.get('DATABASE_URL') } } });
+    // CI環境（swagger.yaml生成時）はDBに接続しない
+    const isSwaggerGeneration = process.env.GENERATE_SWAGGER_YAML === 'true';
+
+    super(
+      isSwaggerGeneration
+        ? {} // DB接続をスキップする
+        : { datasources: { db: { url: config.get<string>('DATABASE_URL') } } },
+    );
   }
 }
